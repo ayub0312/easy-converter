2d00387d488b185b4f48a43c5842286489059686
ayub0312 <64758661+ayub0312@users.noreply.github.com>
2020-05-06 04:04:16 -0700

    Working text calculator
    Got the script to work and evaluate simple math expressions e.g. (3+5)*4/2

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
-var test = 5;
-console.log(test);
+function getMyText() {
+	return document.getElementById("input").value;
+}
+function printResult(){
+	document.getElementById("result").innerText = eval(getMyText());
+}
+document.getElementById("go").addEventListener("click", printResult);
+
+document.getElementById("input").onkeyup = function(e){
+    if(e.keyCode == 13){
+       printResult();
+    }
+}
